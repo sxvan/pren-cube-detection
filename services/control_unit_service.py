7ba695dcba714c1c59c90c2ae95b20bd5828a793
Silvan Büchi <silvan.buechi01@outlook.com>
2024-04-10 23:44:10 +0200

    Implemented uart communication

--- services/control_unit_service.py
+++ services/control_unit_service.py
@@ -1,6 +1,8 @@
 import serial
 from gpiozero import DigitalInputDevice, DigitalOutputDevice
 
+from models.cube_position import CubePosition
+
 
 class ControlUnitService:
     def __init__(self, ready_pin: DigitalOutputDevice, start_pin: DigitalInputDevice, uart_port, baud_rate, encoding,
@@ -28,35 +30,42 @@ class ControlUnitService:
     def wait_for_end_signal(self):
         self.__start_pin.wait_for_inactive()
 
-    # def send_cube_config(self, orientation, cube_config: (CubePosition, str)):
-    #
-    #     # ser = serial.Serial(self.__uart_port, self.__baud_rate)
-    #     data = self.__convert_to_data_string(cube_config)
-    #     data = self.__start_character + data
-    #     data_bytes = data.encode(self.__encoding)
-    #
-    #     crc = self.__get_crc8(data_bytes)
-    #     data_bytes += bytes([crc])
-    #
-    #     # ser.write(data_bytes)
-    #
-    #     # received_data = ser.readline()
-    #
-    # def __get_crc8(self, data):
-    #     crc = 0
-    #     for byte in data:
-    #         crc ^= byte
-    #         for _ in range(8):
-    #             if crc & 0x80:
-    #                 crc = (crc >> 1) ^ self.__crc8_poly
-    #             else:
-    #                 crc <<= 1
-    #             crc &= 0xFF
-    #
-    #     return crc
+    def send_cube_config(self, orientation, cube_config: (CubePosition, str)):
 
-    def __convert_to_data_string(self, cube_dict):
-        sorted_cube_values = [cube_dict[key] for key in sorted(cube_dict.keys(), key=lambda x: x.value)]
+        ser = serial.Serial(self.__uart_port, self.__baud_rate, timeout=self.__retry_delay_ms * 1000)
+        data = self.__get_data_string(cube_config)
+        data = self.__start_character + data
+        data_bytes = data.encode(self.__encoding)
+        crc = self.__get_crc8(data_bytes)
+        data_bytes += bytes([crc])
+
+        tries = 0
+        success = False
+
+        while tries < self.__max_retries and not success:
+            tries = tries + 1
+            ser.write(data_bytes)
+
+            received_data = ser.readline().decode(self.__encoding).strip()
+
+            if received_data == "A":
+                success = True
+
+    def __get_crc8(self, data):
+        crc = 0
+        for byte in data:
+            crc ^= byte
+            for _ in range(8):
+                if crc & 0x80:
+                    crc = (crc >> 1) ^ self.__crc8_poly
+                else:
+                    crc <<= 1
+                crc &= 0xFF
+
+        return crc
+
+    def __get_data_string(self, cube_dict):
+        sorted_cube_values = [cube_dict[key] for key in sorted(cube_dict.keys(), key=lambda x: x.value)] # orientation missing
 
         data = ''
         for color in sorted_cube_values:
