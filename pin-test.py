9c7bcd689c85e438372c8a9e91ee0ea55705d63c
Silvan Buechi <silvan.buechi01@outlook.com>
2024-04-11 09:38:35 +0200

    Test for pin

--- pin-test.py
+++ pin-test.py
@@ -1,19 +1,12 @@
+from time import sleep
+
 from gpiozero import DigitalOutputDevice
 import serial
 
 if __name__ == '__main__':
-    pin1 = DigitalOutputDevice(17)
-    pin2 = DigitalOutputDevice(11)
-    pin3 = DigitalOutputDevice(13)
+    pin1 = DigitalOutputDevice(23)
 
-    pin1.on()
-    pin2.on()
-
-    print(pin1.is_active)
-    print(pin2.is_active)
-    print(pin3.is_active)
-
-    ser = serial.Serial('/dev/serial0', 19200)
-    msg = "Hello World!".encode('utf-8')
-    ser.write(msg)
+    while True:
+        pin1.toggle()
+        sleep(0.2)
 
