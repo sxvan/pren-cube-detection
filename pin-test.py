db471d01f558aae8c21415b4b5ac9c8bee8f16ee
Silvan Buechi <silvan.buechi01@outlook.com>
2024-04-11 10:17:38 +0200

    Test for pin

--- pin-test.py
+++ pin-test.py
@@ -6,10 +6,12 @@ import serial
 if __name__ == '__main__':
     ready = DigitalOutputDevice(23)
     start = InputDevice(24)
+    ser = serial.Serial("/dev/serial0", 19200, timeout=0.2)
 
     while True:
         if start.value:
             ready.toggle()
+            ser.write("@ABC".encode("ascii"))
         sleep(0.2)
 
 
